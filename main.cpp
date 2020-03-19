035693dd555ce18c74190392aeff30dd85c90de2
Bob (NetsvetayDO) <bob@example.com>
2020-03-19 17:11:42 +0300

    code: нахождение минимума

--- main.cpp
+++ main.cpp
@@ -4,11 +4,19 @@ using namespace std;
 
 int main()
 {
-cout << "Enter A and B: ";
-int a, b;
-cin >> a >> b;
-cout << "A + B = " << a + b << '\n'
-     << "A - B = " << a - b << '\n'
-     << "A * B = " << a * b << '\n'
-     << "A / B = " << a / b << '\n'
+    cout << "Enter A and B: ";
+    int a, b;
+    cin >> a >> b;
+    cout << "A + B = " << a + b << '\n'
+         << "A - B = " << a - b << '\n'
+         << "A * B = " << a * b << '\n'
+         << "A / B = " << a / b << '\n'
+         if(a>b)
+    {
+        cout<<b;
+    }
+    else
+    {
+        cout<<a;
+    }
 }
